3108c40e116527eece8a90a15c7d9a8a74bb3eef
valentin127 <143296181+valentin127@users.noreply.github.com>
2024-09-26 19:07:16 -0300

    a

--- Adulto.java
+++ Adulto.java
@@ -3,5 +3,25 @@ public class Adulto extends Vampiro {
     Adulto(String n,int f, int v, int h,String c,boolean a){
         super(n, f, v, h,c, a);
     }
+    @Override
+    public void comer() {
+        if (hambre > 0) {
+            System.out.println(nombre + " está comiendo para saciar su hambre.");
+            hambre -= 20; // Reduce el hambre después de comer
+            if (hambre < 0) {
+                hambre = 0;
+            }
+        } else {
+            System.out.println(nombre + " no tiene hambre en este momento.");
+        }
+    }
 
+    // Implementación del método abstracto habilidadEspecial
+    @Override
+    public void habilidadEspecial() {
+        System.out.println(nombre + " utiliza su habilidad de recién convertido: Velocidad Explosiva.");
+        velocidad += 50; // Aumenta temporalmente la velocidad
+    }
+
+    // Otros métodos específicos para RecienConvertido pueden ir aquí
 }
--- Maduro.java
+++ Maduro.java
@@ -4,4 +4,25 @@ public class Maduro extends Vampiro {
         super(n, f, v, h,c, a);
     }
 
+ @Override
+    public void comer() {
+        if (hambre > 0) {
+            System.out.println(nombre + " está comiendo para saciar su hambre.");
+            hambre -= 20; // Reduce el hambre después de comer
+            if (hambre < 0) {
+                hambre = 0;
+            }
+        } else {
+            System.out.println(nombre + " no tiene hambre en este momento.");
+        }
+    }
+
+    // Implementación del método abstracto habilidadEspecial
+    @Override
+    public void habilidadEspecial() {
+        System.out.println(nombre + " utiliza su habilidad de recién convertido: Velocidad Explosiva.");
+        velocidad += 50; // Aumenta temporalmente la velocidad
+    }
+
+    // Otros métodos específicos para RecienConvertido pueden ir aquí
 }
